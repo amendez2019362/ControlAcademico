8336dcb0e14f9adbe59a5a365131ae5a9ffccf9e
amendez2019362 <amendez-2019362@kinal.edu.gt>
2024-02-16 07:34:44 -0600

    usuario

---
+++ models/usuario.js
@@ -0,0 +1,23 @@
+const { Shema, model } = require('mongoose');
+
+const UsuarioShcema = Shema({
+    nombre: {
+        type: String,
+        requir: [true, 'El nombre es obligatorio']
+    },
+    correo: {
+        type: String,
+        require: [true, 'El correo es obligatorio']
+    },
+    password: {
+        type: String,
+        require: [true, 'El password es obligatorio']
+    },
+    role: {
+        type: String,
+        require: true,
+        enum: ["STUDENT_ROLE", "TEACHER_ROLE"]
+    }
+})
+
+module.exports = model('Usuario', UsuarioSchema);
